fd436a6a7569975fd944837b7f7d3dd39881506e
Leon Rijsdam <leon.rijsdam@outlook.com>
2021-05-11 20:29:33 +0200

    Added class for ean13 and reading csv from fs

--- superpy/functions.py
+++ superpy/functions.py
@@ -1,54 +1,116 @@
 import random
+import csv
+from os.path import abspath, exists, isdir, isfile
 
 
-def generate_ean13(prefix=[]):
-    code = []
+class Ean13Code():
 
-    # convert int or str of digits into list of digits
-    if isinstance(prefix, (str, int)):
-        prefix = list(str(prefix))
+    def __init__(self, prefix=[]):
 
-    # start new ean code with up to 12 digits from prefix
-    if isinstance(prefix, list) and len(prefix) > 0:
-        for digit in prefix:
+        # check prefix type
+        if not isinstance(prefix, (str, int, list)):
+            raise TypeError('Only string, list or int allowed')
 
-            # if code = 12 digits, break
-            if len(code) >= 12:
-                break
+        # convert int or str of digits into list of digits
+        if isinstance(prefix, (str, int)):
+            prefix = list(str(prefix))
 
-            # if digit is int, append
-            if isinstance(digit, int):
-                code.append(digit)
+        # start new ean code with up to 12 digits from prefix
+        code = []
 
-            # if digit is str and is a digit, append as int
-            elif isinstance(digit, str) and digit.isdigit():
-                digit = code.append(int(digit))
+        if isinstance(prefix, list) and len(prefix) > 0:
 
-            # raise TypeError if digit is some other character
-            else:
-                raise TypeError('Only digits allowed')
+            for digit in prefix:
 
-    # add up to 12 random digits to complete code
-    while len(code) < 12:
-        code.append(random.randint(0, 9))
+                # if code = 12 digits, break
+                if len(code) >= 12:
+                    break
 
-    # calculate checksum digit
-    odd = code[0] + code[2] + code[4] + code[6] + code[8] + code[10]
-    even = (code[1] + code[3] + code[5] + code[7] + code[9] + code[11]) * 3
-    units = (odd + even) % 10
-    check = 0
-    if units != 0:
-        check = 10 - units
+                # if digit is int, append
+                if isinstance(digit, int):
+                    code.append(digit)
 
-    # add checksum digit
-    code.append(check)
+                # if digit is str and is a digit, append as int
+                elif isinstance(digit, str):
+                    if digit.isdigit():
+                        digit = code.append(int(digit))
+                    else:
+                        # raise TypeError if digit is some other character
+                        raise TypeError('Only digits allowed')
 
-    # return as string of digits
-    return ''.join([str(digit) for digit in code])
+        # add up to 12 random digits to complete code
+        while len(code) < 12:
+            code.append(random.randint(0, 9))
+
+        self.prefix = prefix
+        self.code = code
+
+    def __str__(self):
+        ean13 = self.code
+        ean13.append(self.calculate_checksum(self.code))
+        return ''.join([str(digit) for digit in ean13])
+
+    def calculate_checksum(self, code):
+        odd = self.code[0] + self.code[2] + self.code[4] + \
+            self.code[6] + self.code[8] + self.code[10]
+        even = (self.code[1] + self.code[3] + self.code[5] +
+                self.code[7] + self.code[9] + self.code[11]) * 3
+        unit = (odd + even) % 10
+        if unit != 0:
+            return 10 - unit
+        return 0
+
+
+class FileSystemDB():
+
+    def __init__(self, filename='', columns=[]):
+        data = []
+
+        if not isinstance(filename, str):
+            raise TypeError('A filename is a string')
+        elif filename == '':
+            raise ValueError('A valid filename is required')
+
+        if not isinstance(columns, list):
+            raise TypeError('A list of column names is required')
+        elif len(columns) == 0:
+            raise ValueError('At least one column is required')
+
+        filepath = abspath(f'./{filename}')
+
+        if not exists(filepath):
+            raise FileExistsError(filepath)
+
+        elif not isfile(filepath):
+            raise FileNotFoundError(filepath)
+
+        else:
+            with open(filepath, mode='r') as csv_file:
+                file_ref = csv.DictReader(
+                    csv_file, delimiter=',', doublequote=True, escapechar='\\', lineterminator='\r\n',
+                    quotechar='"', quoting=csv.QUOTE_MINIMAL, skipinitialspace=True, strict=True)
+                for row in file_ref:
+                    rowdata = {}
+                    for column in columns:
+                        rowdata[column] = row[column]
+                    data.append(rowdata)
+
+            self.data = data
+            self.rowcount = len(data)
+            self.filename = filename
+            self.columns = columns
+            self.columncount = len(columns)
+            self.filepath = filepath
 
 
 def main():
-    pass
+
+    code = Ean13Code('123')
+    print(code)
+
+    products = FileSystemDB('products.csv', ['name', 'department', 'birthday'])
+    print(products.data, products.rowcount, products.filename,
+          products.columns, products.columncount, products.filepath)
 
 
 if __name__ == '__main__':
--- superpy/test_generate_ean13.py
+++ superpy/test_generate_ean13.py
@@ -1,19 +1,26 @@
 import unittest
+import math
 
 from functions import generate_ean13
 
 
 class test_generate_ean13(unittest.TestCase):
 
-    def test1_generate_ean13(self):
+    def test1(self):
         assert generate_ean13(
             [9, 7, 8, 0, 1, 4, 1, 0, 2, 6, 6, 2]) == '9780141026626'
 
-    def test2_generate_ean13(self):
+    def test2(self):
         assert generate_ean13('978014102662') == '9780141026626'
 
-    def test3_generate_ean13(self):
+    def test3(self):
         assert generate_ean13(978014102662) == '9780141026626'
 
-    def test4_generate_ean13(self):
+    def test4(self):
         self.assertRaises(TypeError, generate_ean13, 'abc')
+
+    def test5(self):
+        self.assertRaises(TypeError, generate_ean13, {1})
+
+    def test6(self):
+        self.assertRaises(TypeError, generate_ean13, math.pi)
