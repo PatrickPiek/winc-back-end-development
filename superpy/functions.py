09e93a2843be6f5707cd890a1d74ca429fb68136
Leon Rijsdam <leon.rijsdam@outlook.com>
2021-05-11 15:14:27 +0200

    Update functions

--- superpy/functions.py
+++ superpy/functions.py
@@ -4,37 +4,42 @@ import random
 def generate_ean13(prefix=[]):
     code = []
 
-    if isinstance(prefix, str):
-        prefix = list(prefix)
-
-    if isinstance(prefix, int):
-        prefix = [int(i) for i in str(prefix)]
+    # convert int or str of digits into list of digits
+    if isinstance(prefix, (str, int)):
+        prefix = list(str(prefix))
 
     # start new ean code with up to 12 digits from prefix
     if isinstance(prefix, list) and len(prefix) > 0:
         for digit in prefix:
+
+            # if code = 12 digits, break
             if len(code) >= 12:
                 break
+
+            # if digit is int, append
             if isinstance(digit, int):
                 code.append(digit)
+
+            # if digit is str and is a digit, append as int
             elif isinstance(digit, str) and digit.isdigit():
                 digit = code.append(int(digit))
 
-    # add up to 12 random digits to complete
+    # add up to 12 random digits to complete code
     while len(code) < 12:
         code.append(random.randint(0, 9))
 
-    # calculate and add ‘check digit’
-    # based on: https://www.open.edu/openlearn/science-maths-technology/exploring-communications-technology/content-section-2.1
-
+    # calculate checksum digit
     odd = code[0] + code[2] + code[4] + code[6] + code[8] + code[10]
     even = (code[1] + code[3] + code[5] + code[7] + code[9] + code[11]) * 3
     units = (odd + even) % 10
     check = 0
     if units != 0:
         check = 10 - units
+
+    # add checksum digit
     code.append(check)
 
+    # return as string of digits
     return ''.join([str(digit) for digit in code])
 
 
