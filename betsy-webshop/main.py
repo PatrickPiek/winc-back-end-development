dbcfc8dcb3d44fe3dc23456017242c95eb403b09
Leon Rijsdam <leon.rijsdam@outlook.com>
2021-06-20 12:35:09 +0200

    Update to betsy webshop assignment

--- betsy-webshop/main.py
+++ betsy-webshop/main.py
@@ -1,30 +1,114 @@
 __winc_id__ = "d7b474e9b3a54d23bca54879a4f1855b"
 __human_name__ = "Betsy Webshop"
 
+from models import *
+import peewee
+from peewee import JOIN
+
+
+def main():
+    # create_tables()
+    # insert test data #
+    # test queries #
+    purchase_product(1, 1, 1)
+
+
+def create_tables():
+    with db:
+        db.create_tables(
+            [User,
+             Product,
+             Tag,
+             Transaction,
+             UserProduct,
+             ProductTag
+             ]
+        )
+
 
 def search(term):
-    ...
+    query = (Product
+             .select()
+             .where(
+                 Product.name ** term | Product.description ** term)
+             .order_by(Product.name)
+             ).execute()
 
 
 def list_user_products(user_id):
-    ...
+
+    query = (Product
+             .select()
+             .join(UserProduct, on=(Product.id == UserProduct.product))
+             .join(User, on=(User.id == UserProduct.user))
+             .where(User.id == user_id)
+             ).execute()
 
 
 def list_products_per_tag(tag_id):
-    ...
+    query = (Product
+             .select()
+             .join(ProductTag, on=(Product.id == ProductTag.product))
+             .join(Tag, on=(Tag.id == ProductTag.tag))
+             .where(Tag.id == tag_id)
+             ).execute()
 
 
 def add_product_to_catalog(user_id, product):
-    ...
 
+    query_add_product = Product.create(
+        name=product['name'],
+        description=product['description'],
+        price_per_unit=product['price_per_unit'],
+        quantity_in_stock=product['quantity_in_stock']
+    )
 
-def update_stock(product_id, new_quantity):
-    ...
-
-
-def purchase_product(product_id, buyer_id, quantity):
-    ...
+    UserProduct.create(
+        user=user_id,
+        product=query_add_product.id
+    )
 
 
 def remove_product(product_id):
-    ...
+    query = (UserProduct
+             .delete()
+             .where(UserProduct.product == product_id)
+             .limit(1)
+             ).execute()
+
+
+def update_stock(product_id, new_quantity):
+    query = (Product
+             .update({Product.quantity_in_stock: new_quantity})
+             .where(Product.id == product_id)
+             ).execute()
+
+
+def purchase_product(product_id, buyer_id, quantity):
+    seller = (User
+              .select(User.id)
+              .join(UserProduct, on=(User.id == UserProduct.user))
+              .join(Product, on=(Product.id == UserProduct.product))
+              .where(Product.id == product_id)
+              ).execute()
+
+    product = Product.select().where(Product.id == product_id).execute()
+
+    price_total = product[0].price_per_unit * quantity
+    new_quantity_in_stock = product[0].quantity_in_stock - quantity
+
+    transaction = Transaction.create(
+        buyer_id=buyer_id,
+        seller_id=seller[0].id,
+        quantity=quantity,
+        price_total=price_total)
+
+    update_stock(product_id, new_quantity_in_stock)
+
+    query = (Transaction
+             .select(Transaction.id == transaction.id)
+             ).execute()
+
+
+if __name__ == '__main__':
+    main()
--- betsy-webshop/models.py
+++ betsy-webshop/models.py
@@ -1,1 +1,58 @@
-# Models go here
+from peewee import *
+import datetime
+
+import logging
+logger = logging.getLogger('peewee')
+logger.addHandler(logging.StreamHandler())
+logger.setLevel(logging.DEBUG)
+
+# db = peewee.SqliteDatabase(':memory:')
+db = SqliteDatabase('db.sqlite3', pragmas={'foreign_keys': 1})
+
+
+class BaseModule(Model):
+    class Meta:
+        database = db
+
+
+class Tag(BaseModule):
+    name = CharField()
+
+
+class User(BaseModule):
+    name = CharField()
+    address = CharField()
+    zipcode = CharField()
+    city = CharField()
+    state = CharField()
+    country = CharField()
+    billing_name = CharField()
+    billing_account = CharField()
+    username = CharField()
+    password = CharField()
+
+
+class Product(BaseModule):
+    name = CharField()
+    description = CharField()
+    price_per_unit = IntegerField()
+    quantity_in_stock = IntegerField()
+
+
+class Transaction(BaseModule):
+    buyer = ForeignKeyField(User)
+    seller = ForeignKeyField(User)
+    timestamp = TimestampField(default=datetime.datetime.now)
+    product = ForeignKeyField(Product)
+    quantity = IntegerField()
+    price_total = IntegerField()
+
+
+class UserProduct(BaseModule):
+    user = ForeignKeyField(User, backref='products')
+    product = ForeignKeyField(Product, backref='products')
+
+
+class ProductTag(BaseModule):
+    product = ForeignKeyField(Product, backref='tags')
+    tag = ForeignKeyField(Tag, backref='tags')
